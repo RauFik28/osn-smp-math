87eeff1bbb386452c747e84ce3890d330b23449c
RauFik28 <rfik280502@gmail.com>
2026-02-25 00:03:24 +0700

    Refactor HomeScreen navigation and app bar actions

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,27 +1,51 @@
-
 import 'package:flutter/material.dart';
 import 'quiz_screen.dart';
 import 'history_screen.dart';
 
-Navigator.push(
-  context,
-  MaterialPageRoute(builder: (context) => HistoryScreen()),
-);
-
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(title: Text("Think Math")),
+      appBar: AppBar(
+        title: const Text("Think Math"),
+        actions: [
+
+          IconButton(
+            icon: const Icon(Icons.history),
+            onPressed: () {
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HistoryScreen(),
+                ),
+              );
+
+            },
+          )
+
+        ],
+      ),
+
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.push(context,
-              MaterialPageRoute(builder: (_) => QuizScreen()));
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => QuizScreen(),
+              ),
+            );
+
           },
-          child: Text("Mulai Try Out (20 Soal Random)"),
+          child: const Text("Mulai Try Out (20 Soal Random)"),
         ),
       ),
     );
+
   }
 }
