859e5f8ed9ecbee383f4107a261d24dfed80305a
RauFik28 <rfik280502@gmail.com>
2026-02-25 03:30:12 +0700

    Update question_service.dart

--- lib/services/question_service.dart
+++ lib/services/question_service.dart
@@ -1,1 +1,17 @@
 
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
+class QuestionService {
+  static List<dynamic>? _cachedQuestions;
+
+  static Future<List<dynamic>> getQuestions() async {
+    if (_cachedQuestions != null) {
+      return _cachedQuestions!;
+    }
+
+    final data = await rootBundle.loadString('assets/assets_questions.json');
+    _cachedQuestions = json.decode(data);
+    return _cachedQuestions!;
+  }
+}
