1b30e6a916c206b066401080a43cf860d51aec63
RauFik28 <rfik280502@gmail.com>
2026-02-25 00:52:44 +0700

    Refactor HomeScreen to HistoryScreen with updates

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,42 +1,16 @@
 import 'package:flutter/material.dart';
-import 'screens/quiz_screen.dart';
-import 'screens/history_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HistoryScreen extends StatelessWidget {
+  const HistoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Think Math"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.history),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const HistoryScreen(),
-                ),
-              );
-            },
-          )
-        ],
+        title: const Text("Riwayat Try Out"),
       ),
-
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => QuizScreen(),
-              ),
-            );
-          },
-          child: const Text("Mulai Try Out (20 Soal Random)"),
-        ),
+      body: const Center(
+        child: Text("Belum ada riwayat."),
       ),
     );
   }
