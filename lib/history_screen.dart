a2628d53b57f57a057a22d4d549b9cb64625922f
RauFik28 <rfik280502@gmail.com>
2026-02-24 23:57:50 +0700

    Refactor HistoryScreen to HomeScreen with navigation

--- lib/history_screen.dart
+++ lib/history_screen.dart
@@ -1,44 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
+import 'quiz_screen.dart';
+import 'history_screen.dart';
 
-class HistoryScreen extends StatefulWidget {
-  const HistoryScreen({super.key});
-
-  @override
-  State<HistoryScreen> createState() => _HistoryScreenState();
-}
-
-class _HistoryScreenState extends State<HistoryScreen> {
-  List<Map<String, dynamic>> history = [];
-
-  @override
-  void initState() {
-    super.initState();
-    loadHistory();
-  }
-
-  Future<void> loadHistory() async {
-    final prefs = await SharedPreferences.getInstance();
-    List<String> data = prefs.getStringList("score_history") ?? [];
-
-    setState(() {
-      history = data.map((e) => jsonDecode(e)).toList();
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Riwayat Nilai")),
-      body: ListView.builder(
-        itemCount: history.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text("Skor: ${history[index]["score"]}/20"),
-            subtitle: Text(history[index]["date"]),
-          );
-        },
+      appBar: AppBar(
+        title: const Text("Think Math"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.history),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HistoryScreen(),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => QuizScreen(),
+              ),
+            );
+          },
+          child: const Text("Mulai Try Out (20 Soal Random)"),
+        ),
       ),
     );
   }
